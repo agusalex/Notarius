47b14db38c8b2b812280f2178795c08817472ee2
Agus Alexander <agusalexander@hotmail.com>
2018-02-09 20:34:27 -0300

    fix Db

--- src/main/java/com/Notarius/data/conexion/ConexionHibernate.java
+++ src/main/java/com/Notarius/data/conexion/ConexionHibernate.java
@@ -18,23 +18,23 @@ public class ConexionHibernate {
 		//TODO ver como inyectar esos annotated class o esta seria la forma correcta?
 
        configuration
-/*
+
 		 		.configure()
 				.setProperty(Environment.DIALECT, "org.hibernate.dialect.MariaDB53Dialect")
 				.setProperty(Environment.DRIVER, "org.mariadb.jdbc.Driver")
 				.setProperty(Environment.URL, "jdbc:mariadb://192.168.1.100:3306/Notarius")
 				.setProperty(Environment.USER, "root")
 				.setProperty(Environment.PASS, "")
-				.setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");*/
+				.setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");
 
-
+/*
 		 		.configure()
 				.setProperty(Environment.DIALECT, "org.hibernate.dialect.H2Dialect")
 				.setProperty(Environment.DRIVER, "org.h2.Driver")
 				.setProperty(Environment.URL, "jdbc:h2:~//Notariu;AUTO_SERVER=TRUE")
 				.setProperty(Environment.USER, "root")
 				.setProperty(Environment.PASS, "root")
-				.setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");
+				.setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");*/
 		return configuration;
 	}
 	
