2d5810fb3d2362a38d86b73dbb575af69ba794ff
Agustin Alexander <agustin.alexander@mulesoft.com>
2020-09-01 14:07:56 -0300

    added Dockerfile and parametrized conexion

--- src/main/java/com/Notarius/data/conexion/ConexionHibernate.java
+++ src/main/java/com/Notarius/data/conexion/ConexionHibernate.java
@@ -7,36 +7,50 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.Map;
+
 
 public class ConexionHibernate {
 	private static Configuration configuration = new Configuration();
 	private static SessionFactory sf=null;
-	static boolean  debugDB=true;
+	static boolean  debugDB= false;
+	static String URL= "127.0.0.1:3306/Notarius";
+	static String user= "root";
+	static String pass= "root";
 	
 	
 	private static Configuration getConfiguration() {
 		//TODO ver como inyectar esos annotated class o esta seria la forma correcta?
 
-		if(System.getProperty("os.name").contains("Windows")) {
-			System.out.println("Debug DB");
-			debugDB = true;
+		try {
+			if (System.getenv("DEBUG") != null) {
+				debugDB = System.getenv("DEBUG").equals("true");
+			}
+			if (System.getenv("USER") != null && System.getenv("PASS") != null) {
+				user = System.getenv("USER");
+				pass = System.getenv("PASS");
+			}
+			if (System.getenv("URL") != null) {
+				URL = System.getenv("URL");
+			}
+		}
+		catch (Exception ignored) {
+
 		}
 
-       configuration
-
-		 		.configure()
+       configuration.configure()
 				.setProperty(Environment.DIALECT, "org.hibernate.dialect.MariaDB53Dialect")
 				.setProperty(Environment.DRIVER, "org.mariadb.jdbc.Driver")
-				.setProperty(Environment.URL, "jdbc:mariadb://192.168.1.10:3306/Notarius")
-				.setProperty(Environment.USER, "root")
-				.setProperty(Environment.PASS, "root")
+				.setProperty(Environment.URL, "jdbc:mariadb://"+URL)
+				.setProperty(Environment.USER, user)
+				.setProperty(Environment.PASS, pass)
 				.setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");
 
        if(debugDB){
 		   configuration.configure()
 				   .setProperty(Environment.DIALECT, "org.hibernate.dialect.H2Dialect")
 				   .setProperty(Environment.DRIVER, "org.h2.Driver")
-				   .setProperty(Environment.URL, "jdbc:h2:~//Notariu;AUTO_SERVER=TRUE")
+				   .setProperty(Environment.URL, "jdbc:h2:~//Notarius;AUTO_SERVER=TRUE")
 				   .setProperty(Environment.USER, "root")
 				   .setProperty(Environment.PASS, "root")
 				   .setProperty(Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");
@@ -67,7 +81,8 @@ public class ConexionHibernate {
 				System.out.println("Conexion exitosa");
 				System.out.println("----------------");
 			} catch (Exception e) {
-				System.err.println("Error!!");
+				System.err.println("Error de conexion!!");
+
 				e.printStackTrace();
 			}
 		}
